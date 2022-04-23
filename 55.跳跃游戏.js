d1869d90b6e038db5df2a8fbcd6f249238b07480
= <634605333@qq.com>
2022-04-23 23:13:38 +0800

    pref 跳跃游戏

--- 55.跳跃游戏.js
+++ 55.跳跃游戏.js
@@ -10,18 +10,14 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    if(!nums.length) return false
-    const getMax = (start, end) => {
-        let max = -1
-        for(let i = start; i <= end; i++) {
-            let temp = i + nums[i]
-            if(temp > max) max = temp
-        }
-        if(max === end) return end
-        if(max >= nums.length -1) return max
-        return getMax(end, max)
+    if(nums.length === 1) return true
+    let max = 0
+    for(let i = 0; i <= max; i++) {
+        let temp = i + nums[i]
+        if(temp > max) max = temp
+        if(max >= nums.length - 1) return true
     }
-    return getMax(0, 0) >= nums.length - 1
+    return false
 };
 // @lc code=end
 
