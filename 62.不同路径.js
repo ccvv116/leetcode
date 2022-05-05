2abc916a5c0f5ca1259ba8917074081c037028c8
ccvv116 <634605333@qq.com>
2022-05-06 00:43:52 +0800

    add 62.不同路径添加dp递归写法

--- 62.不同路径.js
+++ 62.不同路径.js
@@ -11,16 +11,27 @@
  * @return {number}
  */
 
+// var uniquePaths = function(m, n) {
+//     const arr = new Array(m).fill(new Array(n))
+//     arr[0].fill(1)
+//     arr.forEach((item) => item[0] = 1)
+//     for(let i = 1; i < m; i++) {
+//         for(let j = 1; j < n; j++) {
+//             arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
+//         }
+//     }
+//     return arr[m-1][n-1]
+// };
+const cache = new Map()
 var uniquePaths = function(m, n) {
-    const arr = new Array(m).fill(new Array(n))
-    arr[0].fill(1)
-    arr.forEach((item) => item[0] = 1)
-    for(let i = 1; i < m; i++) {
-        for(let j = 1; j < n; j++) {
-            arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
-        }
+    const dp = (m,n) => {
+        if(m === 1 || n === 1) return 1
+        if(cache.has(`${m}-${n}`)) return cache.get(`${m}-${n}`)
+        const result = dp(m - 1, n) + dp(m, n -1)
+        cache.set(`${m}-${n}`, result)
+        return result
     }
-    return arr[m-1][n-1]
+    return dp(m, n)
 };
 // @lc code=end
 
