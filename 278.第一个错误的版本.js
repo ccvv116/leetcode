eb072b4ce70ff62ac672d41c4026507630b72fb1
ccvv116 <634605333@qq.com>
2022-06-24 17:11:23 +0800

    add 278.第一个错误版本

---
+++ 278.第一个错误的版本.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=javascript
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 1
+        let end = n
+        while(start < end) {
+            let cur = Math.floor((start + end) / 2)
+            if(isBadVersion(cur)) {
+                end = cur
+            } else {
+                start = cur + 1
+            }
+        }
+        return start
+    };
+};
+// @lc code=end
+
